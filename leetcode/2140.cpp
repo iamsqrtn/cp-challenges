2598de0310e23ec9a9c60c82f09c98acf9866754
iamsqrtn <masterakg2000@gmail.com>
2025-04-02 22:19:32 +0530

    2140. Solving Questions With Brainpower

---
+++ leetcode/2140.cpp
@@ -0,0 +1,29 @@
+#include "stdc++.h"
+
+class Solution {
+public:
+    vector<long long>dp;
+    long long solve(int idx, vector<vector<int>>& questions) {
+        int n=questions.size(),i;
+        long long take=0,notake=0;
+        if(idx>=n)
+            return 0;
+        if(dp[idx]!=-1)
+            return dp[idx];
+        take=questions[idx][0]+solve(idx+questions[idx][1]+1, questions);
+        notake=solve(idx+1, questions);
+        return dp[idx]=max(take, notake);
+    }
+    long long mostPoints(vector<vector<int>>& questions) {
+        int n=questions.size();
+        dp.resize(n, -1);
+        return solve(0, questions);
+    }
+};
+
+int main() {
+    vector<vector<int>>questions={{3,2},{4,3},{4,4},{2,5}};
+    Solution sol;
+    cout<<sol.mostPoints(questions)<<endl;
+    return 0;
+}
